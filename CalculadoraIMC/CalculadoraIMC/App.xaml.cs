73f70ae6188eb242c130daeabbfc787140b89eee
javier barrera <fcbjavier12@hotmail.com>
2023-10-27 22:43:42 -0300

    Agregar archivos de proyecto.

---
+++ CalculadoraIMC/CalculadoraIMC/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace CalculadoraIMC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PaginaPrincipal();
+        }
+        protected override void OnStart() { }
+        protected override void OnSleep() { }
+        protected override void OnResume() { }  
+    }
+}
---
+++ CalculadoraIMC/CalculadoraIMC/PaginaPrincipal.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.Xaml;
+
+namespace CalculadoraIMC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaPrincipal : ContentPage
+    {
+        public PaginaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Altura.Text) &&
+                !string.IsNullOrEmpty(Peso.Text))
+            {
+                var altura = float.Parse(Altura.Text);
+                var peso = float.Parse(Peso.Text);
+
+                var imc = peso / (altura*altura);
+                IMC.Text = imc.ToString();
+
+                string resultado = string.Empty;
+
+                if (imc < 18.5)
+                {
+                    resultado = "Tenes bajo peso";
+                }
+                else if (imc >= 18.5 && imc <= 24.9)
+                {
+                    resultado = "Tu peso es normal";
+                }
+                else if (imc >= 25 && imc <= 29.9)
+                {
+                    resultado = "Tenes sobrepreso";                   
+                }
+                else
+                {
+                    resultado = "Tenes obesidad, ¡Cuídate!";
+                }
+
+                DisplayAlert("Resultado", resultado, "Ok");
+            }
+            else
+            {
+                DisplayAlert("Datos erróneos",
+                    "Por favor, llena toda la información",
+                    "Ok");
+            }
+        }
+
+        private void btLimpiar_Clicked(object sender, EventArgs e)
+        {
+            Altura.Text = string.Empty;
+            IMC.Text = string.Empty;
+            Peso.Text = string.Empty;
+        }
+
+        private void btSalir_Clicked(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }   
+    }
+}
